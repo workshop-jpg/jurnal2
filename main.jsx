e3f8cf19aef53d70903355c94a8697f7b21cce35
workshop-jpg <workshopacc363@gmail.com>
2025-07-27 21:27:15 +0700

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <h1 style={{color: "white", background: "#121212", padding: "20px"}}>Journalses App (Demo)</h1>
+  </React.StrictMode>,
+)
